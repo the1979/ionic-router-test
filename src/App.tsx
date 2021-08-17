5c2ba7071d905c83b4df29a44faaf7e3eab1f17c
Cobus Esterhuyse <cobus.es@gmail.com>
2021-08-17 19:07:09 +0200

    Dummy authentication

--- src/App.tsx
+++ src/App.tsx
@@ -22,23 +22,12 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import AuthExample from './example';
 
 const App: React.FC = () => {
   return (
     <IonApp>
-      <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route path="/" exact={true}>
-              <Redirect to="/page/Inbox" />
-            </Route>
-            <Route path="/page/:name" exact={true}>
-              <Page />
-            </Route>
-          </IonRouterOutlet>
-        </IonSplitPane>
-      </IonReactRouter>
+      <AuthExample/>
     </IonApp>
   );
 };
